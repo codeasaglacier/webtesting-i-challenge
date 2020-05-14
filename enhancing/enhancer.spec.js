f4636b52b30bce7af19e7b05efd34a125935f8ca
codeasaglacier <remingtonshock@gmail.com>
2020-05-13 20:06:19 -0800

    tdd succeed, fail, repair, get

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,64 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe( "jest test", () => {
+  it( "runs tests w/o failing", () => {
+    expect( false ).toBeFalsy()
+  } )
+} )
+
+item = {
+  name: "Staff of the Fainting Goat",
+  durability: 90,
+  enhancement: 4
+}
+
+describe( "enhancer unit tests", () => {
+  it( "repairs item", () => {
+    const { repair } = enhancer
+
+    const mewItem = repair( item )
+    
+    expect( mewItem.durability ).toBe( 100 )
+  } )
+} )
+
+describe( "enhancer unit tests", () => {
+  it( "succeeded enhancement", () => {
+    const { succeed } = enhancer
+  
+    const mewItem = succeed( item )
+    
+    expect( mewItem.enhancement ).toBe( 5 )
+  } )
+} )
+
+describe( "enhancer unit tests", () => {
+  it( "failed enhancement", () => {
+    const { fail } = enhancer
+  
+    const mewItem = fail( item )
+    
+    if ( item.enhancement < 15 ) {
+      expect( mewItem.durability ).toBe( item.durability - 5 )
+    } else if ( item.enhancement >= 15 ) {
+      expect( mewItem.durability ).toBe( item.durability -10 ) 
+    } else if ( item.enhancement > 16 ) {
+      expect( mewItem.enhancement ).toBe( item.enhancement -1 )
+    }
+  } )
+} )
+
+describe( "enhancer unit tests", () => {
+  it( "changes item name", () => {
+    const { get } = enhancer
+  
+    const mewItem = get( item )
+    
+    if ( item.enhancement === 0 ) {
+      expect( mewItem.name).toBe( item.name )
+    } else if ( item.enhancement > 0 ) {
+      expect( mewItem.name ).toBe( item.name ) 
+    }
+  } )
+} )
+//"[+4] Staff of the Fainting Goat"
